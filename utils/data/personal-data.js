a4da3cff1c6c1bd2d731ac13ec4f23b18f3c0740
imvishalksingh <vishal.techengineer@gmail.com>
2024-04-03 02:56:00 +0530

    commit

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,5 +1,5 @@
 export const personalData = {
-  name: "VISHAL KUMAR",
+  name: "VISHAL SINGH",
   profile: '/profile.jpg',
   designation: "Android Developer",
   description: "My name is ViSHAL . I am a enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of android application development and love to make the web more open to the world. My core skill is based on java and kotlin and I love to do most of the things using java. I am available for any kind of job opportunity that suits my skills and interests.",

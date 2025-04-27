73930ece11c013953a8fc74e02730bb499439a31
Vishal Singh <161681190+imvishalksingh@users.noreply.github.com>
2025-04-28 00:40:51 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,10 @@
-// const path = require('path')
- 
-// module.exports = {
-//   sassOptions: {
-//     includePaths: [path.join(__dirname, 'styles')],
-//   },
-//   images: {
-//     domains: ['res.cloudinary.com', 'media.dev.to']
-//   }
-// }
-
-const isProd = process.env.NODE_ENV === "production";
+const path = require('path')
 
 module.exports = {
-  output: "export", // Enables static export
-  basePath: isProd ? "/your-repo-name" : "", // Set GitHub Pages base path
-  images: {
-    unoptimized: true, // Disables Next.js Image Optimization
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
-};
+  images: {
+    domains: ['res.cloudinary.com', 'media.dev.to']
+  }
+}
